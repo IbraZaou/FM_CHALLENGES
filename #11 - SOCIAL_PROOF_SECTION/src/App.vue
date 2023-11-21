4a9d279d6b87b43b7b80644b9b9fc07ca49696b8
IbrahimZaou <ibrahimzaou3@gmail.com>
2023-11-21 14:55:02 +0100

    added project social proof made with vuejs

---
+++ #11 - SOCIAL_PROOF_SECTION/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import Card from './components/Card.vue';
+
+
+</script>
+
+<template>
+  <div class="wrapper">
+
+    <div class="container-text">
+
+      <div class="container-info">
+        <h1><strong>10,000+ of our users love our products.</strong></h1>
+        <p>We only provide great products combined with excellent customer service. See what our satisfied customers are
+          saying about our services.</p>
+      </div>
+
+      <div class="container-rating">
+        <div class="bloc">
+          <img v-for="index in 5" :key="index" src="./assets/images/icon-star.svg" alt="">
+          <p>Rated 5 Stars in Reviews</p>
+        </div>
+
+        <div class="bloc">
+          <img v-for="index in 5" :key="index" src="./assets/images/icon-star.svg" alt="">
+          <p>Rated 5 Stars in Report Guru</p>
+        </div>
+
+        <div class="bloc">
+          <img v-for="index in 5" :key="index" src="./assets/images/icon-star.svg" alt="">
+          <p>Rated 5 Stars in BestTech</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Reviews -->
+
+    <div class="container-review">
+      <Card />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ #11 - SOCIAL_PROOF_SECTION/src/components/Card.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+    data() {
+        return {
+            reviews: [
+                {
+                    id: 1,
+                    name: 'Colton Smith',
+                    role: 'Verified Buyer',
+                    testimony: 'We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!',
+                    img: 'https://randomuser.me/api/portraits/men/1.jpg'
+                },
+                {
+                    id: 2,
+                    name: 'Irene Roberts',
+                    role: 'Verified Buyer',
+                    testimony: 'Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery',
+                    img: 'https://randomuser.me/api/portraits/women/10.jpg'
+                },
+                {
+                    id: 3,
+                    name: 'Anne Wallace',
+                    role: 'Verified Buyer',
+                    testimony: 'Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!',
+                    img: 'https://randomuser.me/api/portraits/women/2.jpg'
+                },
+            ]
+        };
+    }
+};
+</script>
+
+<template>
+    <div class="review-card" v-for="review in reviews" :key="review.id">
+        <div class="container_profile">
+            <img :src="review.img" alt="">
+            <div class="name_role">
+                <p>{{ review.name }}</p>
+                <span>{{ review.role }}</span>
+            </div>
+        </div>
+
+        <p>"{{ review.testimony }}"</p>
+    </div>
+</template>
+
+<style scoped></style>
